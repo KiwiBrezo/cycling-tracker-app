33d7bed5b4b9d6673cc65a243609b01cc7bdbee1
jBreznar <kiwi.breznar@gmail.com>
2023-05-28 21:31:32 +0200

    Added timer functionality.

--- app/src/main/java/si/um/feri/cycling_tracker_app/activites/RideActivity.kt
+++ app/src/main/java/si/um/feri/cycling_tracker_app/activites/RideActivity.kt
@@ -1,19 +1,35 @@
 package si.um.feri.cycling_tracker_app.activites
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.preference.PreferenceManager
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.google.android.material.textview.MaterialTextView
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
 import si.um.feri.cycling_tracker_app.R
+import java.util.concurrent.TimeUnit
 
 
 class RideActivity : AppCompatActivity() {
 
     private lateinit var map : MapView
+    private lateinit var timer : MaterialTextView
+    private lateinit var startStop : Button
+    private lateinit var settingsBtn : ImageView
+
+    private val mInterval = 1000 // 1 second in this case
+    private var mHandler: Handler? = null
+    private var timeInSeconds = 0L
+    private var startButtonClicked = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,9 +50,15 @@ class RideActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_ride)
 
-        map = findViewById<View>(R.id.mapview_new) as MapView
+        this.map = findViewById(R.id.mapview_new)
+        this.startStop = findViewById(R.id.start_stop_btn)
+        this.settingsBtn = findViewById(R.id.settings_btn_ride)
+        this.timer = findViewById(R.id.time_counter_textview)
 
-        bindAndSetUpMap();
+        timer.setText("00:00:00")
+
+        bindAndSetUpMap()
+        bindButtons()
     }
 
     override fun onResume() {
@@ -57,6 +79,11 @@ class RideActivity : AppCompatActivity() {
         map.onPause() //needed for compass, my location overlays, v6.0.0 and up
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        stopTimer()
+    }
+
     private fun bindAndSetUpMap() {
         map.setTileSource(TileSourceFactory.MAPNIK)
         map.setMultiTouchControls(true);
@@ -64,5 +91,97 @@ class RideActivity : AppCompatActivity() {
         mapController.setZoom(16)
         val startPoint = GeoPoint(48.8583, 2.2944)
         mapController.setCenter(startPoint)
+    }
+
+    private fun bindButtons() {
+        startStop.setOnClickListener {
+            startOrStopButtonClicked()
+        }
+    }
+
+    private fun startOrStopButtonClicked() {
+        if (!startButtonClicked) {
+            startTimer()
+            startTimerView()
+        } else {
+            stopTimer()
+            stopTimerView()
+        }
+    }
+
+    private fun startTimer() {
+        mHandler = Handler(Looper.getMainLooper())
+        mStatusChecker.run()
+    }
+
+    private fun stopTimer() {
+        mHandler?.removeCallbacks(mStatusChecker)
+    }
+
+    private fun startTimerView() {
+        startStop.setBackgroundColor(
+            ContextCompat.getColor(
+                this,
+                R.color.secondary_normal
+            )
+        )
+        startStop.setText(R.string.stop_btn_text)
+        startButtonClicked = true
+    }
+
+    private fun stopTimerView() {
+        startStop.setBackgroundColor(
+            ContextCompat.getColor(
+                this,
+                R.color.stop_color
+            )
+        )
+        startStop.setText(R.string.start_btn_text)
+        startButtonClicked = false
+    }
+
+    private var mStatusChecker: Runnable = object : Runnable {
+        override fun run() {
+            try {
+                timeInSeconds += 1
+                updateStopWatchView(timeInSeconds)
+            } finally {
+                // 100% guarantee that this always happens, even if
+                // your update method throws an exception
+                mHandler!!.postDelayed(this, mInterval.toLong())
+            }
+        }
+    }
+
+    private fun updateStopWatchView(timeInSeconds: Long) {
+        val formattedTime = getFormattedStopWatch((timeInSeconds * 1000))
+        //Log.e("formattedTime", formattedTime)
+        timer.setText(formattedTime)
+    }
+
+    fun getFormattedStopWatch(ms: Long): String {
+        var milliseconds = ms
+        val hours = TimeUnit.MILLISECONDS.toHours(milliseconds)
+        milliseconds -= TimeUnit.HOURS.toMillis(hours)
+        val minutes = TimeUnit.MILLISECONDS.toMinutes(milliseconds)
+        milliseconds -= TimeUnit.MINUTES.toMillis(minutes)
+        val seconds = TimeUnit.MILLISECONDS.toSeconds(milliseconds)
+
+        return "${if (hours < 10) "0" else ""}$hours:" +
+                "${if (minutes < 10) "0" else ""}$minutes:" +
+                "${if (seconds < 10) "0" else ""}$seconds"
+    }
+
+    private fun resetTimerView() {
+        timeInSeconds = 0
+        startButtonClicked = false
+        startStop.setBackgroundColor(
+            ContextCompat.getColor(
+                this,
+                R.color.secondary_normal
+            )
+        )
+        startStop.setText(R.string.start_btn_text)
+        timer.setText("00:00:00")
     }
 }

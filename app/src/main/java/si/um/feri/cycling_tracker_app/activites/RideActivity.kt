fa1729d240d28941fc9791da56f3a2fae51767f3
jBreznar <kiwi.breznar@gmail.com>
2023-05-29 23:05:33 +0200

    Added logic for tracking location and showing it on map.

--- app/src/main/java/si/um/feri/cycling_tracker_app/activites/RideActivity.kt
+++ app/src/main/java/si/um/feri/cycling_tracker_app/activites/RideActivity.kt
@@ -1,20 +1,28 @@
 package si.um.feri.cycling_tracker_app.activites
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.preference.PreferenceManager
 import android.view.View
 import android.widget.Button
-import android.widget.EditText
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.material.textview.MaterialTextView
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
 import si.um.feri.cycling_tracker_app.R
 import java.util.concurrent.TimeUnit
 
@@ -23,13 +31,28 @@ class RideActivity : AppCompatActivity() {
 
     private lateinit var map : MapView
     private lateinit var timer : MaterialTextView
-    private lateinit var startStop : Button
+    private lateinit var startPauseBtn : Button
+    private lateinit var stopBtn: Button
     private lateinit var settingsBtn : ImageView
+    private lateinit var locationManager: LocationManager
+
+    private var currentLocation: Location? = null
 
     private val mInterval = 1000 // 1 second in this case
     private var mHandler: Handler? = null
     private var timeInSeconds = 0L
     private var startButtonClicked = false
+    private var hasGps = false
+
+    private val gpsLocationListener: LocationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            currentLocation = location
+        }
+
+        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+        override fun onProviderEnabled(provider: String) {}
+        override fun onProviderDisabled(provider: String) {}
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,11 +74,46 @@ class RideActivity : AppCompatActivity() {
         setContentView(R.layout.activity_ride)
 
         this.map = findViewById(R.id.mapview_new)
-        this.startStop = findViewById(R.id.start_stop_btn)
+        this.startPauseBtn = findViewById(R.id.start_pause_btn)
+        this.stopBtn = findViewById(R.id.stop_btn)
         this.settingsBtn = findViewById(R.id.settings_btn_ride)
         this.timer = findViewById(R.id.time_counter_textview)
 
-        timer.setText("00:00:00")
+        this.locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        this.hasGps = this.locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+
+
+        if (hasGps) {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ),
+                    225
+                )
+                //Toast.makeText(this, "You need this for the app to work!", Toast.LENGTH_SHORT).show()
+                //return
+            }
+
+            this.locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                900,
+                0F,
+                this.gpsLocationListener
+            )
+        }
+
+        timer.text = "00:00:00"
+        stopBtn.visibility = View.GONE;
 
         bindAndSetUpMap()
         bindButtons()
@@ -94,57 +152,64 @@ class RideActivity : AppCompatActivity() {
     }
 
     private fun bindButtons() {
-        startStop.setOnClickListener {
+        startPauseBtn.setOnClickListener {
             startOrStopButtonClicked()
+        }
+
+        stopBtn.setOnClickListener {
+            stopAndResetTimerView()
         }
     }
 
     private fun startOrStopButtonClicked() {
+        stopBtn.visibility = View.VISIBLE;
+
         if (!startButtonClicked) {
             startTimer()
             startTimerView()
         } else {
             stopTimer()
-            stopTimerView()
+            pauseTimerView()
         }
     }
 
     private fun startTimer() {
         mHandler = Handler(Looper.getMainLooper())
-        mStatusChecker.run()
+        timerStatusChecker.run()
     }
 
     private fun stopTimer() {
-        mHandler?.removeCallbacks(mStatusChecker)
+        mHandler?.removeCallbacks(timerStatusChecker)
     }
 
     private fun startTimerView() {
-        startStop.setBackgroundColor(
+        startPauseBtn.setBackgroundColor(
+            ContextCompat.getColor(
+                this,
+                R.color.secondary_dark
+            )
+        )
+        startPauseBtn.setText(R.string.pause_btn_text)
+        startButtonClicked = true
+    }
+
+    private fun pauseTimerView() {
+        startPauseBtn.setBackgroundColor(
             ContextCompat.getColor(
                 this,
                 R.color.secondary_normal
             )
         )
-        startStop.setText(R.string.stop_btn_text)
-        startButtonClicked = true
-    }
-
-    private fun stopTimerView() {
-        startStop.setBackgroundColor(
-            ContextCompat.getColor(
-                this,
-                R.color.stop_color
-            )
-        )
-        startStop.setText(R.string.start_btn_text)
+        startPauseBtn.setText(R.string.start_btn_text)
         startButtonClicked = false
     }
 
-    private var mStatusChecker: Runnable = object : Runnable {
+    private var timerStatusChecker: Runnable = object : Runnable {
         override fun run() {
             try {
                 timeInSeconds += 1
                 updateStopWatchView(timeInSeconds)
+                saveAndShowCurrentLocation()
             } finally {
                 // 100% guarantee that this always happens, even if
                 // your update method throws an exception
@@ -172,16 +237,34 @@ class RideActivity : AppCompatActivity() {
                 "${if (seconds < 10) "0" else ""}$seconds"
     }
 
-    private fun resetTimerView() {
+    private fun stopAndResetTimerView() {
         timeInSeconds = 0
         startButtonClicked = false
-        startStop.setBackgroundColor(
+        startPauseBtn.setBackgroundColor(
             ContextCompat.getColor(
                 this,
                 R.color.secondary_normal
             )
         )
-        startStop.setText(R.string.start_btn_text)
-        timer.setText("00:00:00")
+        startPauseBtn.setText(R.string.start_btn_text)
+        timer.text = "00:00:00"
+        stopBtn.visibility = View.GONE;
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun saveAndShowCurrentLocation() {
+        if (!hasGps) {
+            return
+        }
+
+        currentLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+        if (currentLocation != null) {
+            val startMarker = Marker(map)
+            val geoLocation = GeoPoint(currentLocation!!.latitude, currentLocation!!.longitude)
+            startMarker.position = geoLocation
+            startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+            map.overlays.add(startMarker)
+            map.controller.animateTo(geoLocation)
+        }
     }
 }
